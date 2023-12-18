964c218927736f476b66199a1730fe2c6dfcf722
Henoch <henochyanuar13@gmail.com>
2023-12-18 23:24:43 +0700

    knex configuration

---
+++ knexfile.js
@@ -0,0 +1,21 @@
+// Update with your config settings.
+
+const connection = {
+  client: 'pg',
+  connection: {
+      host: 'localhost',
+      port: '5432',
+      database: 'Thrifting-Shop_Challenge-Gold',
+      user: 'postgres',
+      password: 'telo_godok',
+  }
+}
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: connection,
+  staging: connection,
+  production: connection,
+};
