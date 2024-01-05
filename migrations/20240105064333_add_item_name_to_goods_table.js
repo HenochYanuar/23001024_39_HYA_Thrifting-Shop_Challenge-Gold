7c9b3e37fc0798ac5aa19c25a1c1085829228678
Henoch <henochyanuar13@gmail.com>
2024-01-05 13:49:26 +0700

    create a new migration (add item_name column in goods table)

---
+++ migrations/20240105064333_add_item_name_to_goods_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.alterTable('goods', (table) => {
+    table.string('item_name', 100).notNullable()
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.alterTable('goods', (table) => {
+    table.dropColumn('item_name')
+  })
+};
